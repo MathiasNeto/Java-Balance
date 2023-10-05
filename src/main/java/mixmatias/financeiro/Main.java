64fa6f15992a1dcfdf8c23081b31f696d853e675
Emanuel Matias <matias.neto.edu@gmail.com>
2023-10-05 18:48:38 -0300

    initial commit

---
+++ src/main/java/mixmatias/financeiro/Main.java
@@ -0,0 +1,29 @@
+package mixmatias.financeiro;
+
+import mixmatias.financeiro.enums.TypeBalance;
+import mixmatias.financeiro.interfaces.IBalance;
+import mixmatias.financeiro.models.BalanceFactory;
+import mixmatias.financeiro.models.Product;
+
+import javax.naming.directory.BasicAttribute;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product product = new Product(3, "QUEIJO GRUYERE KG", "P", 21.90);
+
+        List<Product> products = Arrays.asList(product);
+        System.out.println("-----------FINXOLA_SMART------------");
+        BalanceFactory.getBalance(TypeBalance.FINZOLA_SMART).export(products);
+        System.out.println("Produtos exportados para FINXOLA_SMART com sucesso!");
+        System.out.println("-----------TOLEDO_MGV6------------");
+        BalanceFactory.getBalance(TypeBalance.TOLETO_MGV6).export(products);
+        System.out.println("Produtos exportados para TOLEDO_MGV6 com sucesso!");
+        System.out.println("-----------URANO_INTEGRA------------");
+        BalanceFactory.getBalance(TypeBalance.URANO_INTEGRA).export(products);
+        System.out.println("Produtos exportados para URANO_INTEGRA com sucesso!");
+
+    }
+}
