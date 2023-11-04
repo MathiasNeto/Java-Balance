f2f2609727b5d2b7ae522ad5d1a8e371944360e3
Emanuel Matias <matias.neto.edu@gmail.com>
2023-11-03 21:07:00 -0300

    Exception personalized

---
+++ src/main/java/mixmatias/financeiro/exceptions/NullPointerExceptionPersonalized.java
@@ -0,0 +1,7 @@
+package mixmatias.financeiro.exceptions;
+
+public class NullPointerExceptionPersonalized extends NullPointerException{
+    public NullPointerExceptionPersonalized(String s) {
+        super(s);
+    }
+}
